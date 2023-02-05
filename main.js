90848cbc8cf71d9beab353f0d1ca6d6d1d77f9e4
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-02-05 00:04:17 -0800

    Select input field and create function to add todo

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const form = document.querySelector("form");
-
+const inputField = document.querySelector("input");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+
+
 });
+
+function addTodo(inputContent) {
+
+}
