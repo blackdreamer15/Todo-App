3f8f29f46ca0d2694cecde6ab32ff27cb9c77daa
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-02-07 21:14:21 -0800

    Update the addTodo function

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const form = document.querySelector("form");
 const inputField = document.querySelector("input");
+const todoList = document.querySelector("ul#todo-list");
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-
-
+    addTodo(inputField.value);
 });
 
 function addTodo(inputContent) {
+    const newTodoItem = document.createElement("li");
+    newTodoItem.innerText = inputContent;
+    todoList.append(newTodoItem);
+    console.log(newTodoItem.outerHTML);
 
 }
