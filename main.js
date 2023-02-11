3b240b98825b9e264d5f471c956a6ceaf978c269
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-02-11 23:59:22 +0000

    Update - Add major changes to addTodo function

--- main.js
+++ main.js
@@ -1,16 +1,46 @@
 const form = document.querySelector("form");
-const inputField = document.querySelector("input#input-field");
-const todoList = document.querySelector("ul#todo-list");
+const newTodoInput = document.querySelector("input#new-todo-input");
+const todoList = document.querySelector("div#todo-list");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    if (inputField.value.trim().length !== 0) {
-        addTodo(inputField.value);
-    }
-    inputField.value = "";
+    addTodo(newTodoInput.value);
 });
 
 
-function addTodo(todoContent) {
+function addTodo(todoValue) {
+    const todoItemContainer = document.createElement("div");
+    const todoContentContainer = document.createElement("div");
+    const inputCheckbox = document.createElement("input");
+    const todoInputField = document.createElement("input");
+    const todoActionsContainer = document.createElement("div");
+    const editBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button");
 
+    todoItemContainer.classList.add("todo-item");
+    todoContentContainer.classList.add("todo-content");
+    todoActionsContainer.classList.add("actions");
+    editBtn.classList.add("edit");
+    deleteBtn.classList.add("delete");
+    todoInputField.classList.add("todo-input");
+
+    inputCheckbox.setAttribute("type", "checkbox");
+    todoInputField.setAttribute("type", "text");
+    todoInputField.setAttribute("readonly", "readonly");
+
+    editBtn.innerText = "Edit";
+    deleteBtn.innerText = "Delete";
+
+    todoContentContainer.append(inputCheckbox, todoInputField);
+    todoActionsContainer.append(editBtn, deleteBtn);
+    todoItemContainer.append(todoContentContainer, todoActionsContainer);
+
+    if (todoValue.trim().length !== 0) {
+        todoList.append(todoItemContainer);
+        todoInputField.value = todoValue;
+        newTodoInput.value = "";
+    }
+    else {
+        alert("Enter a todo item");
+    }
 }
